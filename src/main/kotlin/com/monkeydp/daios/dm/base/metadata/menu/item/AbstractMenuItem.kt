65e8c04cf4eafaf64b0eae5cceadb7bcefd6899c
iPotato <monkeydpotato@gmail.com>
2019-10-31 21:24:25 +0800

    *

---
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/item/AbstractMenuItem.kt
@@ -0,0 +1,9 @@
+package com.monkeydp.daios.dm.base.metadata.menu.item
+
+import com.monkeydp.daios.dms.sdk.metadata.menu.item.MenuItem
+
+/**
+ * @author iPotato
+ * @date 2019/10/31
+ */
+abstract class AbstractMenuItem : MenuItem
---
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/item/AbstractMenuItemDef.kt
@@ -0,0 +1,10 @@
+package com.monkeydp.daios.dm.base.metadata.menu.item
+
+import com.monkeydp.daios.dms.sdk.metadata.menu.item.MenuItemDef
+
+/**
+ * @author iPotato
+ * @date 2019/10/31
+ */
+abstract class AbstractMenuItemDef : MenuItemDef {
+}

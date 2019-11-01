c189b999a618ade1887046d617c39f7e2b1d5ca1
iPotato <monkeydpotato@gmail.com>
2019-11-01 23:19:53 +0800

    add menu controller

--- src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/api/node/AbstractJdbcNodeApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/api/node/AbstractJdbcNodeApi.kt
@@ -2,7 +2,7 @@ package com.monkeydp.daios.dm.base.jdbc.api.node
 
 import com.monkeydp.daios.dm.base.api.AbstractNodeApi
 import com.monkeydp.daios.dms.sdk.metadata.node.main.Node
-import com.monkeydp.daios.dms.sdk.metadata.node.ctx.NodeLoadContext
+import com.monkeydp.daios.dms.sdk.metadata.node.ctx.NodeLoadCtx
 import com.monkeydp.daios.dms.sdk.metadata.node.def.NodeDef
 
 /**
@@ -11,12 +11,12 @@ import com.monkeydp.daios.dms.sdk.metadata.node.def.NodeDef
  */
 abstract class AbstractJdbcNodeApi : AbstractNodeApi() {
     
-    override fun loadNodes(ctx: NodeLoadContext): List<Node> {
+    override fun loadNodes(ctx: NodeLoadCtx): List<Node> {
         val nodeDef = ctx.path.getLastNodeDef()
         val nodes = mutableListOf<Node>()
         nodeDef.children.forEach { nodes.addAll(loadNodes(ctx, it)) }
         return nodes
     }
     
-    protected abstract fun loadNodes(ctx: NodeLoadContext, def: NodeDef): List<Node>
+    protected abstract fun loadNodes(ctx: NodeLoadCtx, def: NodeDef): List<Node>
 }
--- src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/item/AbstractMi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/item/AbstractMi.kt
@@ -1,5 +1,6 @@
 package com.monkeydp.daios.dm.base.metadata.menu.item
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.monkeydp.daios.dms.sdk.metadata.icon.GlobalIcon.EMPTY_ICON
 import com.monkeydp.daios.dms.sdk.metadata.menu.Menu
 import com.monkeydp.daios.dms.sdk.metadata.menu.item.MenuItem
@@ -11,7 +12,9 @@ import com.monkeydp.daios.dms.sdk.metadata.menu.item.MenuItem
 abstract class AbstractMi : MenuItem {
     override val name by lazy { "${instr.action.fullName} ${instr.target.fullName}" }
     override val icon = EMPTY_ICON
+    @JsonIgnore
     override var menu: Menu? = null
+    override val hasSubmenu by lazy { menu != null }
     
     override fun toString() = name
 }

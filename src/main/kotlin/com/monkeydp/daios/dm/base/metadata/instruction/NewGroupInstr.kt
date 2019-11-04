37f58d9bae090a8af01be27644711214ebb8409a
iPotato <monkeydpotato@gmail.com>
2019-11-04 23:50:29 +0800

    load sub menu

--- src/main/kotlin/com/monkeydp/daios/dm/base/metadata/instruction/NewGroupInstr.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/instruction/NewGroupInstr.kt
@@ -1,6 +1,6 @@
 package com.monkeydp.daios.dm.base.metadata.instruction
 
-import com.monkeydp.daios.dms.sdk.metadata.instruction.action.GlobalAction
+import com.monkeydp.daios.dms.sdk.metadata.instruction.AbstractInstr
 
 /**
  * @author iPotato

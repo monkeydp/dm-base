8979a7a57355e1f0d58bd5247b25a4d78db0da12
iPotato <monkeydpotato@gmail.com>
2019-11-08 17:54:40 +0800

    build form

--- src/main/kotlin/com/monkeydp/daios/dm/base/metadata/form/AbstractCpForm.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/form/AbstractCpForm.kt
@@ -1,17 +1,22 @@
 package com.monkeydp.daios.dm.base.metadata.form
 
+import com.monkeydp.daios.dms.sdk.metadata.JsType.NUMBER
 import com.monkeydp.daios.dms.sdk.metadata.form.CpForm
-import com.monkeydp.daios.dms.sdk.useful.UserInput
-import com.monkeydp.tools.util.JsonUtil
+import com.monkeydp.daios.dms.sdk.metadata.form.item.FormItemInfo
 
 /**
  * @author iPotato
  * @date 2019/10/25
  */
 abstract class AbstractCpForm(
+        @FormItemInfo(label = "Connection Name", required = true)
         override val connName: String,
+        @FormItemInfo(required = true)
         override val host: String,
+        @FormItemInfo(required = true, jsType = NUMBER)
         override val port: String,
+        @FormItemInfo(label = "User Name", required = true)
         override val username: String,
+        @FormItemInfo(required = true)
         override val password: String
 ) : CpForm

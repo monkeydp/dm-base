e2ea65d90dd1ffeb70853f51b3b63ab84ae02042
iPotato <monkeydpotato@gmail.com>
2019-11-30 16:51:38 +0800

    add code generator KotlinPoet

---
+++ src/main/kotlin/com/monkeydp/daios/dm/base/config/AbstractBaseDirpath.kt
@@ -0,0 +1,11 @@
+package com.monkeydp.daios.dm.base.config
+
+import com.monkeydp.daios.dms.sdk.config.AbstractDirpath
+
+/**
+ * @author iPotato
+ * @date 2019/11/30
+ */
+abstract class AbstractBaseDirpath : AbstractDirpath() {
+    var dist = "$main/dist"
+}

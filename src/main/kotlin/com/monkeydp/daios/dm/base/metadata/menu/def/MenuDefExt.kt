a5638f309320dd7522d7ecef9d9847647c961ece
iPotato <monkeydpotato@gmail.com>
2019-12-02 16:40:12 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/def/MenuDefExt.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/menu/def/MenuDefExt.kt
@@ -2,19 +2,12 @@ package com.monkeydp.daios.dm.base.metadata.menu.def
 
 import com.monkeydp.daios.dm.base.metadata.menu.item.def.MenuItemDef
 import com.monkeydp.daios.dm.base.metadata.menu.item.def.StdMid
+import com.monkeydp.tools.ext.initInstance
 
 /**
  * @author iPotato
  * @date 2019/11/29
  */
-fun menu(init: MenuDef.() -> Unit): MenuDef {
-    val menuDef = StdMenuDef()
-    menuDef.init()
-    return menuDef
-}
+fun menu(init: MenuDef.() -> Unit) = initInstance<StdMenuDef>(init)
 
-fun menuItem(init: MenuItemDef.() -> Unit): MenuItemDef {
-    val mid = StdMid()
-    mid.init()
-    return mid
-}
+fun menuItem(init: MenuItemDef.() -> Unit) = initInstance<StdMid>(init)
--- src/main/kotlin/com/monkeydp/daios/dm/base/metadata/node/def/NodeDefExt.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/node/def/NodeDefExt.kt
@@ -2,26 +2,20 @@ package com.monkeydp.daios.dm.base.metadata.node.def
 
 import com.monkeydp.daios.dm.base.metadata.node.def.contract.*
 import com.monkeydp.daios.dm.base.metadata.node.def.std.*
-import kotlin.reflect.full.createInstance
+import com.monkeydp.tools.ext.initInstance
 
 /**
  * @author iPotato
  * @date 2019/11/29
  */
-fun conn(init: ConnNd.() -> Unit) = initNd<StdConnNd>(init)
+fun conn(init: ConnNd.() -> Unit) = initInstance<StdConnNd>(init)
 
-fun db(init: DbNd.() -> Unit) = initNd<StdDbNd>(init)
+fun db(init: DbNd.() -> Unit) = initInstance<StdDbNd>(init)
 
-fun tables(init: TablesNd.() -> Unit) = initNd<StdTablesNd>(init)
+fun tables(init: TablesNd.() -> Unit) = initInstance<StdTablesNd>(init)
 
-fun table(init: TableNd.() -> Unit) = initNd<StdTableNd>(init)
+fun table(init: TableNd.() -> Unit) = initInstance<StdTableNd>(init)
 
-fun views(init: ViewsNd.() -> Unit) = initNd<StdViewsNd>(init)
+fun views(init: ViewsNd.() -> Unit) = initInstance<StdViewsNd>(init)
 
-fun view(init: ViewNd.() -> Unit) = initNd<StdViewNd>(init)
-
-private inline fun <reified Nd : NodeDef> initNd(init: Nd.() -> Unit): Nd {
-    val nd = Nd::class.createInstance()
-    nd.init()
-    return nd
-}
+fun view(init: ViewNd.() -> Unit) = initInstance<StdViewNd>(init)

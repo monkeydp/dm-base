93d45248a7ddd7860922db980263324b322f2150
iPotato <monkeydpotato@gmail.com>
2019-12-14 21:33:31 +0800

    remove DsDefs

--- src/main/kotlin/com/monkeydp/daios/dm/base/datasource/AbstractDsDefs.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dm.base.datasource
-
-import com.monkeydp.daios.dms.sdk.datasource.DsDef
-
-/**
- * @author iPotato
- * @date 2019/12/2
- */
-abstract class AbstractDsDefs<out D : DsDef> : DsDefs<D>
--- src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/datasource/JdbcDsDefs.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dm.base.jdbc.datasource
-
-import com.monkeydp.daios.dm.base.datasource.DsDefs
-
-/**
- * @author iPotato
- * @date 2019/12/9
- */
-interface JdbcDsDefs : DsDefs<JdbcDsDef>

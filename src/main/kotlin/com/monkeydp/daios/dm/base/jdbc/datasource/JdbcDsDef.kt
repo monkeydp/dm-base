1f035f8a62188371a74bf2321e80dd51c463db91
iPotato <monkeydpotato@gmail.com>
2019-12-23 21:59:02 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/datasource/JdbcDsDef.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/datasource/JdbcDsDef.kt
@@ -1,15 +1,31 @@
 package com.monkeydp.daios.dm.base.jdbc.datasource
 
+import com.monkeydp.daios.dms.sdk.datasource.AbstractDsDef
 import com.monkeydp.daios.dms.sdk.datasource.DsDef
 import com.monkeydp.daios.dms.sdk.datasource.DsDriver
-import com.monkeydp.tools.ext.kotlin.initInstance
+import com.monkeydp.daios.dms.sdk.datasource.DsVersion
 
 /**
  * @author iPotato
  * @date 2019/12/3
  */
 interface JdbcDsDef : DsDef {
-    var driver: DsDriver
+    val driver: DsDriver
+    
+    companion object {
+        operator fun invoke(
+                version: DsVersion<*>,
+                driver: DsDriver
+        ): JdbcDsDef = StdJdbcDsDef(version, driver)
+    }
 }
 
-fun jdbcDsDef(init: JdbcDsDef.() -> Unit): JdbcDsDef = initInstance<StdJdbcDsDef>(init)
+abstract class AbstractJdbcDsDef(
+        version: DsVersion<*>,
+        override val driver: DsDriver
+) : JdbcDsDef, AbstractDsDef(version)
+
+private class StdJdbcDsDef(
+        version: DsVersion<*>,
+        driver: DsDriver
+) : AbstractJdbcDsDef(version, driver)
--- src/main/kotlin/com/monkeydp/daios/dm/base/jdbc/datasource/StdJdbcDsDef.kt
+++
@@ -1,8 +0,0 @@
-package com.monkeydp.daios.dm.base.jdbc.datasource
-
-/**
- * @author iPotato
- * @date 2019/12/3
- */
-internal class StdJdbcDsDef : AbstractJdbcDsDef()
-

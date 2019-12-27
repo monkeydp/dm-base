2a2b72d02f02a1075f1cff2536a11fdd87cc1c85
iPotato <monkeydpotato@gmail.com>
2019-12-27 14:25:51 +0800

    add MongodbConnApi

--- src/main/kotlin/com/monkeydp/daios/dm/base/instruction/main/ShowInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.monkeydp.daios.dm.base.instruction.main
-
-import com.monkeydp.daios.dms.sdk.instruction.AbstractInstr
-
-/**
- * @author iPotato
- * @date 2019/10/31
- */
-object ShowInfo : AbstractInstr()

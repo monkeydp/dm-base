0052dd55de1f16716c0f31c88ba24be4f2fb0e39
iPotato <monkeydpotato@gmail.com>
2019-12-28 15:35:28 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractNodeApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractNodeApi.kt
@@ -1,9 +1,22 @@
 package com.monkeydp.daios.dm.base.api
 
+import com.monkeydp.daios.dm.base.metadata.node.def.NodeDefStruct
+import com.monkeydp.daios.dm.base.metadata.node.def.find
+import com.monkeydp.daios.dm.base.metadata.node.def.sub.ConnNd
 import com.monkeydp.daios.dms.sdk.api.NodeApi
+import com.monkeydp.daios.dms.sdk.conn.ConnProfile
+import com.monkeydp.daios.dms.sdk.metadata.node.ConnNode
+import com.monkeydp.daios.dms.sdk.share.kodein.dmKodeinRepo
+import com.monkeydp.daios.dms.sdk.share.kodein.findImpl
 
 /**
  * @author iPotato
  * @date 2019/10/25
  */
-abstract class AbstractNodeApi : NodeApi
+abstract class AbstractNodeApi : NodeApi {
+    
+    protected val ndStruct: NodeDefStruct get() = dmKodeinRepo.findImpl()
+    
+    override fun loadConnNodes(cps: Iterable<ConnProfile>): List<ConnNode> =
+            ndStruct.find<ConnNd>().run { cps.map { create(it) } }
+}
---
+++ src/main/kotlin/com/monkeydp/daios/dm/base/metadata/node/def/UnhandledNodeDefException.kt
@@ -0,0 +1,9 @@
+package com.monkeydp.daios.dm.base.metadata.node.def
+
+import com.monkeydp.tools.exception.inner.InnerException
+
+/**
+ * @author iPotato
+ * @date 2019/12/28
+ */
+class UnhandledNodeDefException(def: NodeDef) : InnerException("Unhandled node def: `${def.javaClass.name}`")

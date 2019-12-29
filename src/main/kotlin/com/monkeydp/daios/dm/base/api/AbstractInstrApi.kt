6fe7c73eb242638d40fb50e1f5e05a9c3c2a1827
iPotato <monkeydpotato@gmail.com>
2019-12-29 13:18:28 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractInstrApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractInstrApi.kt
@@ -1,9 +1,18 @@
 package com.monkeydp.daios.dm.base.api
 
 import com.monkeydp.daios.dms.sdk.api.InstrApi
+import com.monkeydp.daios.dms.sdk.instruction.InstrParser
+import com.monkeydp.daios.dms.sdk.instruction.InstrParsingCtx
+import com.monkeydp.daios.dms.sdk.share.kodein.dmKodeinRepo
+import com.monkeydp.daios.dms.sdk.share.kodein.findImpl
 
 /**
  * @author iPotato
  * @date 2019/11/5
  */
-abstract class AbstractInstrApi : InstrApi
+abstract class AbstractInstrApi : InstrApi {
+    override fun parse(ctx: InstrParsingCtx) {
+        val parser: InstrParser = dmKodeinRepo.findImpl(tag = ctx.instr)
+        parser.parse(ctx)
+    }
+}

779b547eb2f463e030aeaf7b3dd325c28b414f11
iPotato <monkeydpotato@gmail.com>
2019-12-29 13:07:19 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractFormApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractFormApi.kt
@@ -1,9 +1,22 @@
 package com.monkeydp.daios.dm.base.api
 
 import com.monkeydp.daios.dms.sdk.api.FormApi
+import com.monkeydp.daios.dms.sdk.instruction.Instruction
+import com.monkeydp.daios.dms.sdk.metadata.form.Form
+import com.monkeydp.daios.dms.sdk.metadata.form.FormBuilder
+import com.monkeydp.daios.dms.sdk.received.form.ReceivedForm
+import com.monkeydp.daios.dms.sdk.share.kodein.dmKodeinRepo
+import com.monkeydp.daios.dms.sdk.share.kodein.findImpl
+import kotlin.reflect.KClass
 
 /**
  * @author iPotato
  * @date 2019/10/28
  */
-abstract class AbstractFormApi : FormApi
+abstract class AbstractFormApi : FormApi {
+    
+    private val receivedFormKClass: KClass<ReceivedForm> get() = dmKodeinRepo.findImpl()
+    
+    override fun loadFrom(instruction: Instruction): Form =
+            FormBuilder.buildForm(receivedFormKClass)
+}

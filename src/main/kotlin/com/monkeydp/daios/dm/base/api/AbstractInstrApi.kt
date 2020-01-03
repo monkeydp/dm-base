bbda1611f4d1b39740eda59313d0644534ae65d5
iPotato <monkeydpotato@gmail.com>
2020-01-03 17:27:45 +0800

    *

--- src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractInstrApi.kt
+++ src/main/kotlin/com/monkeydp/daios/dm/base/api/AbstractInstrApi.kt
@@ -4,13 +4,13 @@ import com.monkeydp.daios.dms.sdk.api.InstrApi
 import com.monkeydp.daios.dms.sdk.dm.dmKodeinRepo
 import com.monkeydp.daios.dms.sdk.dm.findImpl
 import com.monkeydp.daios.dms.sdk.instruction.InstrParser
-import com.monkeydp.daios.dms.sdk.instruction.InstrParsingCtx
+import com.monkeydp.daios.dms.sdk.instruction.InstrParsingParams
 
 /**
  * @author iPotato
  * @date 2019/11/5
  */
 abstract class AbstractInstrApi : InstrApi {
-    override fun parse(ctx: InstrParsingCtx) =
-            dmKodeinRepo.findImpl<InstrParser>(tag = ctx.instr).parse(ctx)
+    override fun parse(params: InstrParsingParams) =
+            dmKodeinRepo.findImpl<InstrParser>(tag = params.instr).parse(params)
 }
